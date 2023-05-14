eec9ad1c44a89f438a6a8a96ce97e8ff87e40180
EC2 Default User <ec2-user@ip-172-31-82-119.ec2.internal>
2023-05-14 03:58:00 +0000

    cleared unnecessary files

--- mnist_with_cnn.py
+++
@@ -1,9 +0,0 @@
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.utils import np_utils
-
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32')
-X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32')
